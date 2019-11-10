4c857840a4ad079115f2764836c578352ed569e7
Yu Ishihara <yu_ishihara.a2@keio.jp>
2019-11-10 15:25:31 +0900

    implement transition function

--- chapter3/ideal_robot.py
+++ chapter3/ideal_robot.py
@@ -25,3 +25,28 @@ class IdealRobot(Drawable):
             xy=(x, y), radius=self._radius, fill=False, color=self._color)
         drawn_objects.append(ax.add_patch(robot))
         return drawn_objects
+
+    @classmethod
+    def transition_function(cls, vel, omega, delta_t, pose):
+        theta = pose[2]
+        if math.fabs(omega) < 1e-10:
+            delta_x = vel * math.cos(theta) * delta_t
+            delta_y = vel * math.sin(theta) * delta_t
+            delta_theta = omega * delta_t
+        else:
+            delta_x = vel/omega * \
+                (math.sin(theta + omega * delta_t) - math.sin(theta))
+            delta_y = -vel/omega * \
+                (math.cos(theta + omega * delta_t) - math.cos(theta))
+            delta_theta = omega * delta_t
+        delta_pose = np.array([delta_x, delta_y, delta_theta])
+        return pose + delta_pose
+
+
+if __name__ == "__main__":
+    print(IdealRobot.transition_function(
+        0.1, 0.0, 1.0, np.array([0, 0, 0]).T))
+    print(IdealRobot.transition_function(
+        0.1, 10.0/180*math.pi, 9.0, np.array([0, 0, 0]).T))
+    print(IdealRobot.transition_function(
+        0.1, 10.0/180*math.pi, 18.0, np.array([0, 0, 0]).T))
